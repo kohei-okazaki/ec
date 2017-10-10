a672dba05a9304f695fc19291ffe42ba54fa3e18
kohei.okazaki <kohei.okazaki@dena.jp>
2017-10-10 10:27:49 +0900

    log出力用クラスを利用するように修正

--- ec-site/src/main/java/jp/co/site/controller/LoginController.java
+++ ec-site/src/main/java/jp/co/site/controller/LoginController.java
@@ -3,13 +3,12 @@ package jp.co.site.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import jp.co.site.log.EcLogger;
 import jp.co.site.view.View;
 import jp.co.site.web.session.EcSiteSessionKey;
 import jp.co.site.web.session.EcsiteSessionManager;
@@ -22,8 +21,6 @@ import jp.co.site.web.session.EcsiteSessionManager;
 @Controller
 public class LoginController {
 
-	private final Logger LOG = LoggerFactory.getLogger(this.getClass().getSimpleName());
-
 	/**
 	 * ログイン画面<br>
 	 * ログイン画面描画時にセッション情報を削除<br>
@@ -34,7 +31,7 @@ public class LoginController {
 	@RequestMapping(value = "/login.html", method = RequestMethod.GET)
 	public String login(Model model, HttpServletRequest request) {
 
-		LOG.info(this.getClass().getSimpleName() + "#login");
+		EcLogger.getInstance().info(this.getClass(), " # login");
 
 		HttpSession session = request.getSession();
 		EcsiteSessionManager.getInstance().removeKey(session, EcSiteSessionKey.SEQ_CUSTOMER_ID);
--- ec-site/src/main/java/jp/co/site/log/EcLogger.java
+++ ec-site/src/main/java/jp/co/site/log/EcLogger.java
@@ -5,7 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class EcLogger {
 
-	/** singeton */
+	/** singleton */
 	private static EcLogger instance = new EcLogger();
 
 	/**
